f1d856881792db90e1b522d8893d6184636b40ac
Ivan Shetska <vanya.shetko38@gmail.com>
2022-08-05 08:15:04 +0300

    feat: add flashing with a timer
    Using a hardware timer I implemented the task of blinking the LED at intervals of 200ms on, 100ms off.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -89,7 +89,7 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_TIM_Base_Start_IT(&htim1); //запуск таймера
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -171,7 +171,7 @@ static void MX_TIM1_Init(void)
   htim1.Instance = TIM1;
   htim1.Init.Prescaler = 719;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 10000;
+  htim1.Init.Period = 10000-1;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
